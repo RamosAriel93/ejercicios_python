dca51738f5d3c8f1db25c1e34dcc416bb5e3d158
gvilaro-at-522001166142 <gabriela.vilaro@tecso.coop>
2020-09-24 21:25:44 -0300

    first commit

---
+++ ejercicio_1.py
@@ -0,0 +1,26 @@
+# edad = int(input('Ingrese su edad: '))
+#
+# if edad >= 18:
+#     print("si puede ingresar")
+# else:
+#     print('fuera')
+
+# dato = input('Ingrese dato: ')
+#
+# list = ['posho', 'luna', 'lola', 'posha']
+#
+# if list.count(dato) > 0:
+#     print('El dato existe', dato)
+# else:
+#     print('El dato no existe')
+
+# if dato in list:
+#     print('El dato existe', dato)
+# else:
+#     print('El dato no existe')
+
+valor1 = int(input('Ingrese el primer valor: '))
+valor2 = int(input('Ingrese el segundo valor: '))
+suma = valor1+valor2
+print('El resultado de la suma es :', suma)
+
